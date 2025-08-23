f82f59e18ff73f4dd2a1c7419f55fea91324407e
Abinashaayan <abinash.sinha@aayaninfotech.com>
2025-08-23 14:35:29 +0530

    sdffdf

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Modal, Carousel } from "react-bootstrap";
 import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import certificate from "../../Assets/61f89754-40bd-4822-bb3d-e9756b5eeb7c.JPEG";
 import Toolstack from "./Toolstack";
+import certificate1 from "../../Assets/61f89754-40bd-4822-bb3d-e9756b5eeb7c.JPEG";
+import certificate2 from "../../Assets/javascript(basic).png";
+import certificate3 from "../../Assets/coursera2-aws_page-0001.jpg";
 
 function About() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const certificates = [
+    { id: 1, src: certificate1, alt: "Certificate 1", title: "HackerRank Problem Solving" },
+    { id: 2, src: certificate2, alt: "Certificate 2", title: "JavaScript Basic Certificate" },
+    { id: 3, src: certificate3, alt: "Certificate 3", title: "AWS Cloud Fundamentals" },
+    // Add more certificates here
+  ];
+
+  const handleShow = (index) => {
+    setSelectedIndex(index);
+    setShowModal(true);
+  };
+
+  const handleClose = () => setShowModal(false);
+
   return (
     <Container fluid className="about-section">
       <Particle />
@@ -27,24 +46,163 @@ function About() {
             </h1>
             <Aboutcard />
           </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={certificate} alt="about" className="img-fluid" />
+          <Col md={5} style={{ paddingTop: "120px", paddingBottom: "50px" }} className="about-img">
+            <h3 style={{ color: "white", marginBottom: "20px", textAlign: "center" }}>
+              My <span className="purple">Certifications</span>
+            </h3>
+
+            <Row className="justify-content-center">
+              {certificates.map((cert, index) => (
+                <Col xs={6} md={6} key={cert.id} className="mb-4 text-center">
+                  <div
+                    className="certificate-thumbnail"
+                    onClick={() => handleShow(index)}
+                    style={{
+                      cursor: "pointer",
+                      transition: "transform 0.3s ease",
+                      borderRadius: "10px",
+                      overflow: "hidden",
+                      boxShadow: "0 5px 15px rgba(0, 0, 0, 0.3)"
+                    }}
+                    onMouseEnter={(e) => {
+                      e.currentTarget.style.transform = "scale(1.05)";
+                    }}
+                    onMouseLeave={(e) => {
+                      e.currentTarget.style.transform = "scale(1)";
+                    }}
+                  >
+                    <img
+                      src={cert.src}
+                      alt={cert.alt}
+                      className="img-fluid"
+                      style={{
+                        width: "100%",
+                        height: "150px",
+                        objectFit: "cover",
+                        border: "2px solid rgba(138, 43, 226, 0.3)",
+                        borderRadius: "10px"
+                      }}
+                    />
+                  </div>
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
-        {/* <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1> */}
 
         <Techstack />
-
-        {/* <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1> */}
         <Toolstack />
+
+        {/* Modal for enlarged view - Placed outside the Row to prevent rendering issues */}
+        <Modal
+          show={showModal}
+          onHide={handleClose}
+          size="lg"
+          centered
+          style={{
+            backdropFilter: "blur(5px)"
+          }}
+        >
+          <Modal.Header 
+            closeButton 
+            style={{ 
+              background: "linear-gradient(45deg, #6c5ce7, #8A2BE2)", 
+              color: "white",
+              border: "none"
+            }}
+          >
+            <Modal.Title>My Certifications</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{ padding: "0" }}>
+            <Carousel
+              activeIndex={selectedIndex}
+              onSelect={setSelectedIndex}
+              interval={null}
+              prevIcon={
+                <span 
+                  aria-hidden="true" 
+                  style={{ 
+                    fontSize: "2rem", 
+                    color: "#8A2BE2",
+                    background: "rgba(255, 255, 255, 0.7)",
+                    borderRadius: "50%",
+                    width: "40px",
+                    height: "40px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}
+                >
+                  ‹
+                </span>
+              }
+              nextIcon={
+                <span 
+                  aria-hidden="true" 
+                  style={{ 
+                    fontSize: "2rem", 
+                    color: "#8A2BE2",
+                    background: "rgba(255, 255, 255, 0.7)",
+                    borderRadius: "50%",
+                    width: "40px",
+                    height: "40px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}
+                >
+                  ›
+                </span>
+              }
+            >
+              {certificates.map((cert) => (
+                <Carousel.Item key={cert.id}>
+                  <img
+                    className="d-block w-100"
+                    src={cert.src}
+                    alt={cert.alt}
+                    style={{
+                      borderRadius: "0 0 5px 5px"
+                    }}
+                  />
+                  <Carousel.Caption style={{
+                    background: "rgba(0, 0, 0, 0.6)",
+                    borderRadius: "5px",
+                    left: "50%",
+                    right: "50%",
+                    transform: "translateX(-50%)",
+                    bottom: "10px",
+                    width: "fit-content",
+                    padding: "5px 15px",
+                    maxWidth: "80%"
+                  }}>
+                    <h5>{cert.title}</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Modal.Body>
+        </Modal>
+
+        <style>
+          {`
+            .certificate-thumbnail:hover {
+              transform: scale(1.05);
+            }
+            
+            .carousel-control-prev, .carousel-control-next {
+              width: 5%;
+            }
+            
+            .carousel-control-prev {
+              left: 10px;
+            }
+            
+            .carousel-control-next {
+              right: 10px;
+            }
+          `}
+        </style>
 
         {/* <Github /> */}
       </Container>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,48 +15,67 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em", textAlign:"justify" }}>
+            <h1 style={{ fontSize: "2.6em", textAlign: "justify" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I'm a passionate <span className="purple">Full-Stack Developer</span> with over 3 years of experience
+              specializing in the <span className="purple">MERN stack</span> and modern web technologies.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I'm proficient in
               <i>
-                <b className="purple"> Javascript and TypeScript </b>
+                <b className="purple"> JavaScript, TypeScript, </b>
+              </i>
+              and have extensive experience with
+              <i>
+                <b className="purple"> React.js, Next.js, Vue.js, and Node.js</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My expertise includes building
               <i>
-                <b className="purple">Web Technologies 
-                  {/* and Products  */}
-                  </b> 
-                  {/* and also in areas related to{" "} */}
-                {/* <b className="purple">
-                  Blockchain.
-                </b> */}
+                <b className="purple"> scalable web applications </b>
+              </i>
+              with
+              <i>
+                <b className="purple"> 80%+ reusable components</b>
+              </i>
+              , integrating
+              <i>
+                <b className="purple"> third-party APIs</b>
+              </i>
+              , and implementing
+              <i>
+                <b className="purple"> payment systems with Stripe and PayPal</b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              I've successfully delivered projects that resulted in
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript and TypeScript Library and Frameworks
-                </b>
+                <b className="purple"> 30% faster development cycles</b>
               </i>
-              &nbsp; like
+              ,
               <i>
-                <b className="purple"> React.js, Next.js and Vue.js</b>
+                <b className="purple"> 40% performance improvements</b>
               </i>
+              , and
+              <i>
+                <b className="purple"> 25% reduction in bug rates</b>
+              </i>
+              through automated testing
+              <br />
+              <br />
+              I'm passionate about creating
+              <i>
+                <b className="purple"> efficient, user-focused solutions</b>
+              </i>
+              and continuously exploring new technologies to enhance my skill set
             </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img style={{borderRadius:"50%"}} src="https://media.licdn.com/dms/image/v2/D4D03AQFdOqHAfs_6QQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1709892867050?e=1755734400&v=beta&t=fciCget5jDaJyxuT5GaWXAIKHSpe54JybGsL51KfgBY" className="img-fluid" alt="avatar" />
+              <img style={{ borderRadius: "50%" }} src="https://media.licdn.com/dms/image/v2/D4D03AQFdOqHAfs_6QQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1709892867050?e=1755734400&v=beta&t=fciCget5jDaJyxuT5GaWXAIKHSpe54JybGsL51KfgBY" className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
@@ -77,16 +96,6 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://twitter.com/Abinash4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li> */}
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/sinha2/"
@@ -97,16 +106,6 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/Abinash4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li> */}
             </ul>
           </Col>
         </Row>
